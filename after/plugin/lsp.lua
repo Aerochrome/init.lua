73b08eedc9632cacdcceee74582402200ad6abe7
Philipp Lätsch <p.laetsch@outlook.de>
2023-10-08 17:32:05 +0200

    :sparkles: Added lua lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -17,7 +17,7 @@ end)
 
 require('mason').setup({})
 require('mason-lspconfig').setup({
-	ensure_installed = {'tsserver', 'rust_analyzer', 'phpactor'},
+	ensure_installed = {'tsserver', 'rust_analyzer', 'phpactor', 'lua_ls'},
 	handlers = {
 		lsp_zero.default_setup,
 		lua_ls = function()
