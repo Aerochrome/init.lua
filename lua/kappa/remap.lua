dc92a5bdaec0d7494dafb9f586aa63b38a0c30e5
Philipp Lätsch <p.laetsch@outlook.de>
2023-10-08 21:37:21 +0200

    :sparkles: Added Remaps for Centering Cursor

--- lua/kappa/remap.lua
+++ lua/kappa/remap.lua
@@ -1,2 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Keep Cursor Centered on page up and down
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-f>", "<C-f>zz")
+vim.keymap.set("n", "<C-b>", "<C-b>zz")
