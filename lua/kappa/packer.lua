310b8f72db2f11d7b4e45707512f1c4c13bdf473
Philipp Lätsch <p.laetsch@outlook.de>
2023-10-14 21:46:45 +0200

    feat: Added tokyonight theme

--- lua/kappa/packer.lua
+++ lua/kappa/packer.lua
@@ -14,6 +14,7 @@ return require('packer').startup(function(use)
   }
 
   use {'morhetz/gruvbox', config = function() vim.cmd.colorscheme("gruvbox") end }
+  use{'folke/tokyonight.nvim', config = function() vim.cmd.colorscheme("tokyonight") end }
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 
